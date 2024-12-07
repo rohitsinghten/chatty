e6536ee0378c523d47fcf71f249a0cd05bac3c94
Rohit <rs3412@srmist.edu.in>
2024-12-07 14:27:31 +0530

    __dirname bug fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,6 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use('/api/messages',messageRoutes)
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
